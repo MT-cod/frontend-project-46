5b01d5425c6c50389fb89c23a56378eb3b088960
“Константин <mammmoth@list.ru>
2022-11-09 00:43:33 +0300

    3

---
+++ tests/FormattersTest.php
@@ -0,0 +1,316 @@
+<?php
+
+//Модуль тестирования форматтеров вывода результата работы программы
+
+namespace Differ\Differ;
+
+use PHPUnit\Framework\TestCase;
+
+class FormattersTest extends TestCase
+{
+    private array $genDiffResultArr;
+    private string $stylishFormatTestResult;
+    private string $plainFormatTestResult;
+    private string $jsonFormatTestResult;
+
+    /**
+     * @dataProvider rightResults
+     */
+    public function testFormatOutput(
+        string $rightResultOfStylishFormatOutput,
+        string $rightResultOfPlainFormatOutput,
+        string $rightResultOfJsonFormatOutput,
+        string $genDiffRightResultInJson
+    ) {
+        $this->genDiffResultArr = json_decode($genDiffRightResultInJson, true);
+
+        $this->stylishFormatTestResult = resultArrayToResultString($this->genDiffResultArr, 'stylish');
+        $this->assertEquals($rightResultOfStylishFormatOutput, $this->stylishFormatTestResult);
+        $this->plainFormatTestResult = resultArrayToResultString($this->genDiffResultArr, 'plain');
+        $this->assertEquals($rightResultOfPlainFormatOutput, $this->plainFormatTestResult);
+        $this->jsonFormatTestResult = resultArrayToResultString($this->genDiffResultArr, 'json');
+        $this->assertEquals($rightResultOfJsonFormatOutput, $this->jsonFormatTestResult);
+    }
+
+    public function rightResults()
+    {
+        return [
+            ['{
+    common: {
+      + follow: false
+        setting1: Value 1
+      - setting2: 200
+      - setting3: true
+      + setting3: null
+      + setting4: blah blah
+      + setting5: {
+            key5: value5
+        }
+        setting6: {
+            doge: {
+              - wow: 
+              + wow: so much
+            }
+            key: value
+          + ops: vops
+        }
+    }
+    group1: {
+      - baz: bas
+      + baz: bars
+        foo: bar
+      - nest: {
+            key: value
+        }
+      + nest: str
+    }
+  - group2: {
+        abc: 12345
+        deep: {
+            id: 45
+        }
+    }
+  + group3: {
+        deep: {
+            id: {
+                number: 45
+            }
+        }
+        fee: 100500
+    }
+}', "Property 'common.follow' was added with value: false
+Property 'common.setting2' was removed
+Property 'common.setting3' was updated. From true to null
+Property 'common.setting4' was added with value: 'blah blah'
+Property 'common.setting5' was added with value: [complex value]
+Property 'common.setting6.doge.wow' was updated. From '' to 'so much'
+Property 'common.setting6.ops' was added with value: 'vops'
+Property 'group1.baz' was updated. From 'bas' to 'bars'
+Property 'group1.nest' was updated. From [complex value] to 'str'
+Property 'group2' was removed
+Property 'group3' was added with value: [complex value]", '[
+    {
+        "nodeKey": "common",
+        "child": [
+            {
+                "nodeKey": "follow",
+                "nodeValue": false,
+                "diffStatus": "added"
+            },
+            {
+                "nodeKey": "setting1",
+                "nodeValue": "Value 1",
+                "diffStatus": "unchanged"
+            },
+            {
+                "nodeKey": "setting2",
+                "nodeValue": 200,
+                "diffStatus": "deleted"
+            },
+            {
+                "nodeKey": "setting3",
+                "nodeValueOld": true,
+                "nodeValueNew": null,
+                "diffStatus": "updated"
+            },
+            {
+                "nodeKey": "setting4",
+                "nodeValue": "blah blah",
+                "diffStatus": "added"
+            },
+            {
+                "nodeKey": "setting5",
+                "nodeValue": {
+                    "key5": "value5"
+                },
+                "diffStatus": "added"
+            },
+            {
+                "nodeKey": "setting6",
+                "child": [
+                    {
+                        "nodeKey": "doge",
+                        "child": [
+                            {
+                                "nodeKey": "wow",
+                                "nodeValueOld": "",
+                                "nodeValueNew": "so much",
+                                "diffStatus": "updated"
+                            }
+                        ]
+                    },
+                    {
+                        "nodeKey": "key",
+                        "nodeValue": "value",
+                        "diffStatus": "unchanged"
+                    },
+                    {
+                        "nodeKey": "ops",
+                        "nodeValue": "vops",
+                        "diffStatus": "added"
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        "nodeKey": "group1",
+        "child": [
+            {
+                "nodeKey": "baz",
+                "nodeValueOld": "bas",
+                "nodeValueNew": "bars",
+                "diffStatus": "updated"
+            },
+            {
+                "nodeKey": "foo",
+                "nodeValue": "bar",
+                "diffStatus": "unchanged"
+            },
+            {
+                "nodeKey": "nest",
+                "nodeValueOld": {
+                    "key": "value"
+                },
+                "nodeValueNew": "str",
+                "diffStatus": "updated"
+            }
+        ]
+    },
+    {
+        "nodeKey": "group2",
+        "nodeValue": {
+            "abc": 12345,
+            "deep": {
+                "id": 45
+            }
+        },
+        "diffStatus": "deleted"
+    },
+    {
+        "nodeKey": "group3",
+        "nodeValue": {
+            "deep": {
+                "id": {
+                    "number": 45
+                }
+            },
+            "fee": 100500
+        },
+        "diffStatus": "added"
+    }
+]', '[
+    {
+        "nodeKey": "common",
+        "child": [
+            {
+                "nodeKey": "follow",
+                "nodeValue": false,
+                "diffStatus": "added"
+            },
+            {
+                "nodeKey": "setting1",
+                "nodeValue": "Value 1",
+                "diffStatus": "unchanged"
+            },
+            {
+                "nodeKey": "setting2",
+                "nodeValue": 200,
+                "diffStatus": "deleted"
+            },
+            {
+                "nodeKey": "setting3",
+                "nodeValueOld": true,
+                "nodeValueNew": null,
+                "diffStatus": "updated"
+            },
+            {
+                "nodeKey": "setting4",
+                "nodeValue": "blah blah",
+                "diffStatus": "added"
+            },
+            {
+                "nodeKey": "setting5",
+                "nodeValue": {
+                    "key5": "value5"
+                },
+                "diffStatus": "added"
+            },
+            {
+                "nodeKey": "setting6",
+                "child": [
+                    {
+                        "nodeKey": "doge",
+                        "child": [
+                            {
+                                "nodeKey": "wow",
+                                "nodeValueOld": "",
+                                "nodeValueNew": "so much",
+                                "diffStatus": "updated"
+                            }
+                        ]
+                    },
+                    {
+                        "nodeKey": "key",
+                        "nodeValue": "value",
+                        "diffStatus": "unchanged"
+                    },
+                    {
+                        "nodeKey": "ops",
+                        "nodeValue": "vops",
+                        "diffStatus": "added"
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        "nodeKey": "group1",
+        "child": [
+            {
+                "nodeKey": "baz",
+                "nodeValueOld": "bas",
+                "nodeValueNew": "bars",
+                "diffStatus": "updated"
+            },
+            {
+                "nodeKey": "foo",
+                "nodeValue": "bar",
+                "diffStatus": "unchanged"
+            },
+            {
+                "nodeKey": "nest",
+                "nodeValueOld": {
+                    "key": "value"
+                },
+                "nodeValueNew": "str",
+                "diffStatus": "updated"
+            }
+        ]
+    },
+    {
+        "nodeKey": "group2",
+        "nodeValue": {
+            "abc": 12345,
+            "deep": {
+                "id": 45
+            }
+        },
+        "diffStatus": "deleted"
+    },
+    {
+        "nodeKey": "group3",
+        "nodeValue": {
+            "deep": {
+                "id": {
+                    "number": 45
+                }
+            },
+            "fee": 100500
+        },
+        "diffStatus": "added"
+    }
+]
+']];
+    }
+}
